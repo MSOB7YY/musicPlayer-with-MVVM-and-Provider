8884bd7e701688d6868d58b6cf271490f0dab91d
pranavpv773 <pranavpv773@gmail.com>
2022-08-06 22:01:48 +0530

    unwanted files deleted

--- music_player_app/lib/playlist/view_model/fuctions/playlist_functions.dart
+++ music_player_app/lib/playlist/view_model/fuctions/playlist_functions.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: invalid_use_of_visible_for_testing_member, invalid_use_of_protected_member
-
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:music_player_app/all_songs/view_model/allsongs_provider.dart';
