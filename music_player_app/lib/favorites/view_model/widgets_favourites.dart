60aad52033634f45866f9dadbea483a7ebacefc4
pranavpv773 <pranavpv773@gmail.com>
2022-08-08 14:16:56 +0530

    alert box for favorites seperate widget created

--- music_player_app/lib/favorites/view_model/widgets_favourites.dart
+++ music_player_app/lib/favorites/view_model/widgets_favourites.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:music_player_app/favorites/view_model/favorites_function.dart';
-import 'package:music_player_app/utilities/view/core.dart';
-import 'package:provider/provider.dart';
+import 'package:music_player_app/favorites/view/widgets/delete_alert.dart';
 
 class FavoritesWidget with ChangeNotifier {
   showFavoriteDeletionBox(BuildContext context, int index) {
@@ -12,46 +10,6 @@ class FavoritesWidget with ChangeNotifier {
           index: index,
         );
       },
-    );
-  }
-}
-
-class DeletionAlertBox extends StatelessWidget {
-  const DeletionAlertBox({Key? key, required this.index}) : super(key: key);
-  final int index;
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      backgroundColor: kWhite,
-      content: const Text(
-        'Do you want to remove song from favorites?',
-        style: TextStyle(
-          color: Colors.grey,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: const Text('Cancel'),
-        ),
-        TextButton(
-          onPressed: () {
-            context.read<FavoriteFunctions>().deletion(index, context);
-            const snackBar = SnackBar(
-              content: Text('Remove from favourites'),
-            );
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-            Navigator.of(context).pop();
-          },
-          child: const Text(
-            'Remove',
-            style: TextStyle(color: Colors.amber),
-          ),
-        )
-      ],
     );
   }
 }
